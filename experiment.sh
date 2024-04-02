58d060733f6292783e46cb315f6b19e07cac0e7e
BogdanTurbal <bogdan.turbal.y@gmail.com>
2024-04-02 03:38:33 +0300

    Mod bash 2

--- experiment.sh
+++ experiment.sh
@@ -18,17 +18,26 @@ cd /gpfs/work4/0/tese0660/projects/transf_learn_attack
 conda activate att
 
 CONT_PATH = /gpfs/work4/0/tese0660/projects/transf_learn_attack/
-python /gpfs/work4/0/tese0660/projects/transf_learn_attack/attack_trans.py CONT_PATH --msl 2 --ne 2 --mae 1024 --mel 0 --seed 1 --run 0 --mod_id 0&
-python /gpfs/work4/0/tese0660/projects/transf_learn_attack/attack_trans.py CONT_PATH --msl 2 --ne 2 --mae 1024 --mel 0 --seed 42 --run 1 --mod_id 0&
-python /gpfs/work4/0/tese0660/projects/transf_learn_attack/attack_trans.py CONT_PATH --msl 2 --ne 2 --mae 1024 --mel 0 --seed 1234 --run 2 --mod_id 0&
 
-python /gpfs/work4/0/tese0660/projects/transf_learn_attack/attack_trans.py CONT_PATH --msl 2 --ne 2 --mae 1024 --mel 0 --seed 1 --run 0 --mod_id 1&
-python /gpfs/work4/0/tese0660/projects/transf_learn_attack/attack_trans.py CONT_PATH --msl 2 --ne 2 --mae 1024 --mel 0 --seed 42 --run 1 --mod_id 1&
-python /gpfs/work4/0/tese0660/projects/transf_learn_attack/attack_trans.py CONT_PATH --msl 2 --ne 2 --mae 1024 --mel 0 --seed 1234 --run 2 --mod_id 1&
+mkdir {CONT_PATH}/0_id/
+mkdir {CONT_PATH}/1_id/
+mkdir {CONT_PATH}/2_id/
+mkdir {CONT_PATH}/3_id/
+mkdir {CONT_PATH}/4_id/
+mkdir {CONT_PATH}/5_id/
+mkdir {CONT_PATH}/6_id/
+mkdir {CONT_PATH}/7_id/
+mkdir {CONT_PATH}/8_id/
 
-python /gpfs/work4/0/tese0660/projects/transf_learn_attack/attack_trans.py CONT_PATH --msl 2 --ne 2 --mae 1024 --mel 0 --seed 1 --run 0 --mod_id 2&
-python /gpfs/work4/0/tese0660/projects/transf_learn_attack/attack_trans.py CONT_PATH --msl 2 --ne 2 --mae 1024 --mel 0 --seed 42 --run 1 --mod_id 2&
-python /gpfs/work4/0/tese0660/projects/transf_learn_attack/attack_trans.py CONT_PATH --msl 2 --ne 2 --mae 1024 --mel 0 --seed 1234 --run 2 --mod_id 2&
+python /gpfs/work4/0/tese0660/projects/transf_learn_attack/attack_trans.py {CONT_PATH}/0_id/ --msl 2 --ne 2 --mae 1024 --mel 0 --seed 1 --run 0 --mod_id 0&
+python /gpfs/work4/0/tese0660/projects/transf_learn_attack/attack_trans.py {CONT_PATH}/1_id/ --msl 2 --ne 2 --mae 1024 --mel 0 --seed 42 --run 1 --mod_id 0&
+python /gpfs/work4/0/tese0660/projects/transf_learn_attack/attack_trans.py {CONT_PATH}/2_id/ --msl 2 --ne 2 --mae 1024 --mel 0 --seed 1234 --run 2 --mod_id 0&
+python /gpfs/work4/0/tese0660/projects/transf_learn_attack/attack_trans.py {CONT_PATH}/3_id/ --msl 2 --ne 2 --mae 1024 --mel 0 --seed 1 --run 0 --mod_id 1&
+python /gpfs/work4/0/tese0660/projects/transf_learn_attack/attack_trans.py {CONT_PATH}/4_id/ --msl 2 --ne 2 --mae 1024 --mel 0 --seed 42 --run 1 --mod_id 1&
+python /gpfs/work4/0/tese0660/projects/transf_learn_attack/attack_trans.py {CONT_PATH}/5_id/ --msl 2 --ne 2 --mae 1024 --mel 0 --seed 1234 --run 2 --mod_id 1&
+python /gpfs/work4/0/tese0660/projects/transf_learn_attack/attack_trans.py {CONT_PATH}/6_id/ --msl 2 --ne 2 --mae 1024 --mel 0 --seed 1 --run 0 --mod_id 2&
+python /gpfs/work4/0/tese0660/projects/transf_learn_attack/attack_trans.py {CONT_PATH}/7_id/ --msl 2 --ne 2 --mae 1024 --mel 0 --seed 42 --run 1 --mod_id 2&
+python /gpfs/work4/0/tese0660/projects/transf_learn_attack/attack_trans.py {CONT_PATH}/8_id/ --msl 2 --ne 2 --mae 1024 --mel 0 --seed 1234 --run 2 --mod_id 2&
 
 wait
 
